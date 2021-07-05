330f57369aca4c67de7d1d0aa0516b0e3c834eb8
SHREYAAS1000 <75013816+SHREYAAS1000@users.noreply.github.com>
2021-07-05 16:07:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload(){
+image = loadImage("https://i.postimg.cc/T20G74pm/m.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses); // on(): used to start  execueting
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is loaded");
+}
+
+function draw()
+{
+//image(video,0,0,300,300)
+}
+             
+function take_snapshot()
+{
+    save("MyFilterImage.png");
+}
+noseX=0;
+noseY=0;
+function gotPoses(results)
+{
+     if(results.length > 0) // if condition is not used , then posenet will stop and app will not work
+     {
+         console.log(results);
+         noseX=results[0].pose.nose.x;
+         noseY=results[0].pose.nose.y;
+         console.log("nose x ="+ results[0].pose.nose.x);
+         console.log("nose y ="+ results[0].pose.nose.y);
+     }
+}
+
